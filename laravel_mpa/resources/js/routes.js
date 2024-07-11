f3a41995f41147234c2b5a3cf153d2e2b0fe1bd5
Hammad Hassan <46231833+hammadshahir@users.noreply.github.com>
2024-07-11 13:14:43 +0200

    Implemented basic routes functionality

--- laravel_mpa/resources/js/routes.js
+++ laravel_mpa/resources/js/routes.js
@@ -1,8 +1,39 @@
 import Dashboard from "./components/Dashboard.vue";
+import ListAppointments from "./pages/appointments/ListAppointments.vue";
+import UserProfile from "./pages/profile/UserProfile.vue";
+import Settings from "./pages/Settings/Settings.vue";
+import ListUsers from "./pages/users/ListUsers.vue";
+
+
+
 export default [
     {
         path: '/admin/dashboard',
         name: 'admin.dashboard',
         component: Dashboard,
-    }
+    },
+
+    {
+        path: '/admin/appointments',
+        name: 'admin.appointments',
+        component: ListAppointments,
+    },
+
+    {
+        path: '/admin/users',
+        name: 'admin.users',
+        component: ListUsers,
+    },
+
+    {
+        path: '/admin/settings',
+        name: 'admin.settings',
+        component: Settings,
+    },
+
+    {
+        path: '/admin/profile',
+        name: 'admin.profile',
+        component: UserProfile,
+    },
 ]
--- laravel_mpa/resources/views/admin/layouts/app.blade.php
+++ laravel_mpa/resources/views/admin/layouts/app.blade.php
@@ -7,8 +7,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Admin Area | Laravel Multipurpose Application</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js']);
-
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
         <body class="hold-transition sidebar-mini">
             <div class="wrapper">
                 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -21,6 +21,9 @@
                         </li>
                         <li class="nav-item d-none d-sm-inline-block">
                             <a href="#" class="nav-link">Contact</a>
+                        </li>
+                        <li class="nav-item d-none d-sm-inline-block">
+                            <a href="#" class="nav-link">More Links</a>
                         </li>
                     </ul>
 
@@ -138,63 +141,59 @@
                 </nav>
 
                 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-                    <a href="index3.html" class="brand-link">
-                        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8;" />
-                        <span class="brand-text font-weight-light">AdminLTE 3</span>
+                    <a href="/admin/dashboard" class="brand-link">
+                        {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8;" /> --}}
+                        <span class="brand-text font-weight-light"><strong>Laravel MPA</strong></span>
                     </a>
 
                     <div class="sidebar">
-                        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                            <div class="image">
-                                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
-                            </div>
-                            <div class="info">
-                                <a href="#" class="d-block">Alexander Pierce</a>
-                            </div>
-                        </div>
-
-                        <div class="form-inline">
-                            <div class="input-group" data-widget="sidebar-search">
-                                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-                                <div class="input-group-append">
-                                    <button class="btn btn-sidebar">
-                                        <i class="fas fa-search fa-fw"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
                         <nav class="mt-2">
                             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                                <li class="nav-item menu-open">
-                                    <a href="#" class="nav-link active">
+                                <li class="nav-item">
+                                    <a href ="/admin/dashboard" class="nav-link">
                                         <i class="nav-icon fas fa-tachometer-alt"></i>
                                         <p>
-                                            Starter Pages
-                                            <i class="right fas fa-angle-left"></i>
+                                            Dashboard
                                         </p>
                                     </a>
-                                    <ul class="nav nav-treeview">
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link active">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p>Active Page</p>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="#" class="nav-link">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p>Inactive Page</p>
-                                            </a>
-                                        </li>
-                                    </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-th"></i>
+                                    <a href="/admin/appointments" active-class="active" class="nav-link">
+                                        <i class="nav-icon fas fa-calendar"></i>
                                         <p>
-                                            Simple Link
-                                            <span class="right badge badge-danger">New</span>
+                                            Appointments
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/admin/users" active-class="active" class="nav-link">
+                                        <i class="nav-icon fas fa-users"></i>
+                                        <p>
+                                            Users
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/admin/settings" active-class="active" class="nav-link">
+                                        <i class="nav-icon fas fa-cog"></i>
+                                        <p>
+                                            Settings
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/admin/profile" active-class="active" class="nav-link">
+                                        <i class="nav-icon fas fa-user"></i>
+                                        <p>
+                                            Profile
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" active-class="active" class="nav-link">
+                                        <i class="nav-icon fas fa-stop"></i>
+                                        <p>
+                                            Logout
                                         </p>
                                     </a>
                                 </li>
@@ -223,6 +222,4 @@
                 </footer>
             </div>
         </body>
-    </head>
-
 </html>
